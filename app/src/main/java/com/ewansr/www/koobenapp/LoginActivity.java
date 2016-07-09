d8028cbe833875b7ec899a1b711bff854b5f3b95
ewansr <ewansr@gmail.com>
2016-07-08 20:06:38 -0500

    getDataFacebook
    obtención de datos del api de FB

--- app/src/main/java/com/ewansr/www/koobenapp/LoginActivity.java
+++ app/src/main/java/com/ewansr/www/koobenapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.ewansr.www.koobenapp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,14 +19,19 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.larvalabs.svgandroid.SVG;
-import com.larvalabs.svgandroid.SVGParser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
-import mehdi.sakout.fancybuttons.FancyButton;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.ewansr.www.koobenapp.cUtils.setStatusColor;
 
@@ -35,7 +43,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private Context context;
     private TextView tvRegister;
-    private FancyButton btnLogin;
+    private TextView tvNameFB;
+    private CircleImageView imgFB;
+    private Button btnLogin;
     private EditText mail;
     private EditText password;
     private LoginButton facebookLogin;
@@ -52,11 +62,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setStatusColor( LoginActivity.this );
 
         context = LoginActivity.this;
-        btnLogin = (FancyButton) findViewById( R.id.btnLogin );
+        btnLogin = (Button) findViewById( R.id.btnLogin );
         mail = (EditText) findViewById( R.id.edtUsuario );
         password = (EditText) findViewById( R.id.edtContrasena );
         tvRegister = (TextView) findViewById(R.id.tvRegister);
+        tvNameFB = (TextView) findViewById(R.id.tvNameFB);
         facebookLogin = (LoginButton) findViewById(R.id.facebookLoginButton);
+        imgFB = (CircleImageView) findViewById(R.id.imgFB);
 
         btnLogin.setOnClickListener( this );
         tvRegister.setOnClickListener( this );
@@ -65,20 +77,94 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.d( "edmsamuel", "Login con Facebook exitoso" );
-            }
+                Log.i( "edmsamuel", "Login con Facebook exitoso." );
+                ProgressDialog progressDialog = null;
 
-            @Override
-            public void onCancel() {
-                Log.i( "edmsamuel", "Login con Facebook cancelado" );
-            }
+                try {
+                    progressDialog = new ProgressDialog(LoginActivity.this);
+                    progressDialog.setMessage("Leyendo datos de Facebook");
+                    progressDialog.show();
 
-            @Override
-            public void onError(FacebookException error) {
-                Log.e( "edmsamuel", "Login con Facebook error" );
-            }
-        });
+                    String accessToken = loginResult.getAccessToken().getToken();
+                    Log.i("accessToken", accessToken);
+
+                    //** twichy Checar https://developers.facebook.com/docs/graph-api/*/
+                    GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            Log.i("LoginActivity", response.toString());
+                            // Obtener datos de facebook
+                            Bundle bFacebookData = getFacebookData(object, imgFB);
+
+                            // Aquí tus variables pedorras
+                            // que me solicitaste
+                            String idFB    = bFacebookData.getString("id");
+                            String nameFB  = bFacebookData.getString("first_name");
+                            String emailFb = bFacebookData.getString("email");
+                            tvNameFB.setText(nameFB);
+                        }
+                    });
+
+                    // Contenedor de los datos que requerimos
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location"); // Parámetros que pedimos a facebook
+                    request.setParameters(parameters);
+                    request.executeAsync();
+                }finally{
+                    if (progressDialog.isShowing()){
+                        progressDialog.dismiss();
+                    }
+                }
+        }
+
+        @Override
+        public void onCancel() {
+            Log.i( "edmsamuel", "Login con Facebook cancelado" );
+        }
+
+        @Override
+        public void onError(FacebookException exception) {
+            System.out.println("onError");
+            Log.v("LoginActivity", exception.getCause().toString());
+        }
+    });
     }
+
+    private Bundle getFacebookData(JSONObject object, ImageView imgProfile) {
+            Bundle bundle = new Bundle();
+            String id = null;
+            try {
+                id = object.getString("id");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        String UrlImgProfile = "https://graph.facebook.com/" + id + "/picture?width=200&height=150";
+        bundle.putString("profile_pic", UrlImgProfile);
+        LoadRemoteImg rimg = new LoadRemoteImg(imgProfile);
+        rimg.execute(UrlImgProfile);
+
+        // Lo siguiente está de más explicarlo con manzanas
+        bundle.putString("idFacebook", id);
+            try {
+                if (object.has("first_name"))
+                    bundle.putString("first_name", object.getString("first_name"));
+                if (object.has("last_name"))
+                    bundle.putString("last_name", object.getString("last_name"));
+                if (object.has("email"))
+                    bundle.putString("email", object.getString("email"));
+                if (object.has("gender"))
+                    bundle.putString("gender", object.getString("gender"));
+                if (object.has("birthday"))
+                    bundle.putString("birthday", object.getString("birthday"));
+                if (object.has("location"))
+                    bundle.putString("location", object.getJSONObject("location").getString("name"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return bundle;
+        }
 
 
     /**
@@ -88,8 +174,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mail.setText( "" );
         password.setText( "" );
     }
-
-
 
     /**
      * Llamado en caso que la autenticación sea exitosa.
@@ -125,7 +209,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
 
-
     /**
      * Llamado al ocurrir un evento de click a un elemento en la vista.
      *
@@ -136,11 +219,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         // en caso que el objetivo del `click` del usuario sea el botón de logueo
         if ( target.getId() == R.id.btnLogin ) {
-            APILoginAuth autenticacion = new APILoginAuth(){
-                @Override public void autenticacionExitosa( APILoginAuthModel usuario ) { loginAutenticacionExitosa( usuario ); }
-                @Override public void autenticacionFallida() { loginAautenticacionFallida(); }
-            };
-            autenticacion.autenticar( mail.getText().toString(), password.getText().toString() );
+            ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+            progressDialog.setMessage("Procesando datos...");
+            progressDialog.show();
+            try {
+                APILoginAuth autenticacion = new APILoginAuth() {
+                    @Override
+                    public void autenticacionExitosa(APILoginAuthModel usuario) {
+                        loginAutenticacionExitosa(usuario);
+                    }
+
+                    @Override
+                    public void autenticacionFallida() {
+                        loginAautenticacionFallida();
+                    }
+                };
+                autenticacion.autenticar(mail.getText().toString(), password.getText().toString());
+            }finally {
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+            }
         }
 
         if ( target.getId() == R.id.tvRegister ) {
@@ -148,7 +247,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(i);
         }
     }
-
 
     @Override
     protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
